a555e59c8a48e6b242809e293503fb1c938966bc
tyshaikh <ty@k2datascience.com>
2018-03-19 13:09:20 -0400

    update text

--- app.py
+++ app.py
@@ -29,11 +29,8 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
                     response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                    # send_message(recipient_id, response_sent_text)
+                    send_message(recipient_id, message['message'].get('text'))
     return "Message Processed"
 
 
